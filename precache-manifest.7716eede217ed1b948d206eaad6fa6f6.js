269ad3bc0f1d45dcbdd7ab21c0ad601dd642a173
senniladri35 <senniladri35@gmail.com>
2020-06-15 13:18:12 +0530

    Updates

---
+++ precache-manifest.7716eede217ed1b948d206eaad6fa6f6.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef18c4a473782bb192f99505dcfccbf1",
+    "url": "/movies-info/index.html"
+  },
+  {
+    "revision": "1e75d943c7d442b0f80f",
+    "url": "/movies-info/static/css/2.87791eef.chunk.css"
+  },
+  {
+    "revision": "32640e807a32e8eec41d",
+    "url": "/movies-info/static/css/main.d20d7e65.chunk.css"
+  },
+  {
+    "revision": "1e75d943c7d442b0f80f",
+    "url": "/movies-info/static/js/2.82a259f0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movies-info/static/js/2.82a259f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32640e807a32e8eec41d",
+    "url": "/movies-info/static/js/main.570ad196.chunk.js"
+  },
+  {
+    "revision": "52f6c0230713a0953f76",
+    "url": "/movies-info/static/js/runtime-main.8feb52ee.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/movies-info/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/movies-info/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/movies-info/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/movies-info/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/movies-info/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
